2445e39239ad91b35f145eefd8406abacac9fdd0
Weblate Admin <weblate@theyosh.nl>
2021-02-02 12:51:18 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (44 of 44 strings)
    
    Translation: WiFiKilL3r/interface
    Translate-URL: https://weblate.theyosh.nl/projects/wifikill3r/interface/nl/

--- translations/WiFiKilL3r-nl.ts
+++ translations/WiFiKilL3r-nl.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="nl">
 <context>
     <name>AboutPage</name>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="20"/>
         <source>About WiFiKilL3r</source>
-        <translation type="unfinished"></translation>
+        <translation>Over WiFiKilL3r</translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="24"/>
         <source>WiFiKilL3r</source>
-        <translation type="unfinished"></translation>
+        <translation>WiFiKilL3r</translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="37"/>
@@ -29,7 +29,20 @@ On the coverpage you have the option to reconnect your WiFi, and enable/disable 
 This app is created in order to protect your mobile device from being tracked by wifitrackers in citymalls and other public locations. The idea is that when you leave your trusted networks, there is no need to keep WiFi running. So this app will shutdown the WiFi device for you. This way it will save battery power, but more important, you can&apos;t be tracked!
 
 WiFiKilL3r app is created by TheYOSH https://theyosh.nl</source>
-        <translation type="unfinished"></translation>
+        <translation>Deze app controleert de wifi-status van uw apparaat en schakelt deze uit wanneer uw telefoon niet is verbonden met een vertrouwd netwerk. Er wordt een melding weergegeven wanneer uw wifi wordt uitgeschakeld.
+
+- U kunt wifi in- of uitschakelen door op de wifi-indicator te tikken
+- U kunt de WiFi KilL3r-daemon in- of uitschakelen door op de KilL3r-indicator te tikken
+- De app toont de laatste speelduur onderaan de app
+
+Gebruik het menu-item &amp;apos;Vertrouwde netwerken beheren&amp;apos; in het hoofdmenu om vertrouwde wifi-netwerken toe te voegen of te verwijderen. Zorg ervoor dat wifi is ingeschakeld!. Tik op de wifi-naam om toe te voegen aan of te verwijderen uit de vertrouwde lijst. Updates zijn direct!
+
+Op de voorpagina heb je de mogelijkheid om je wifi opnieuw te verbinden en de achtergrond daemon in / uit te schakelen
+
+*Waarom*
+Deze app is gemaakt om te voorkomen dat uw mobiele apparaat wordt gevolgd door wifitrackers in stadscentra en andere openbare locaties. Het idee is dat wanneer u uw vertrouwde netwerken verlaat, het niet nodig is om wifi actief te houden. Dus deze app zal het wifi-apparaat voor je afsluiten. Op deze manier bespaart u batterijvermogen, maar wat nog belangrijker is, u kunt niet worden gevolgd!
+
+WiFiKilL3r app is gemaakt door TheYOSH https://theyosh.nl</translation>
     </message>
 </context>
 <context>
@@ -37,17 +50,17 @@ WiFiKilL3r app is created by TheYOSH https://theyosh.nl</source>
     <message>
         <location filename="../qml/cover/CoverPage.qml" line="15"/>
         <source>WiFiKilL3r</source>
-        <translation type="unfinished"></translation>
+        <translation>WiFiKilL3r</translation>
     </message>
     <message>
         <location filename="../qml/cover/CoverPage.qml" line="22"/>
         <source>Last check</source>
-        <translation type="unfinished"></translation>
+        <translation>Laatste controle</translation>
     </message>
     <message>
         <location filename="../qml/cover/CoverPage.qml" line="22"/>
         <source>Loading...</source>
-        <translation type="unfinished"></translation>
+        <translation>Laden...</translation>
     </message>
 </context>
 <context>
@@ -55,82 +68,82 @@ WiFiKilL3r app is created by TheYOSH https://theyosh.nl</source>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="13"/>
         <source>About WiFiKilL3r</source>
-        <translation type="unfinished"></translation>
+        <translation>Over WiFiKilL3r</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="17"/>
         <source>Manage trusted networks</source>
-        <translation type="unfinished"></translation>
+        <translation>Beheer vertrouwde netwerken</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="21"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Instellingen</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="37"/>
         <source>WiFiKilL3r</source>
-        <translation type="unfinished"></translation>
+        <translation>WiFiKilL3r</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="54"/>
         <source>Current status</source>
-        <translation type="unfinished"></translation>
+        <translation>Huidige status</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="63"/>
         <source>WiFi</source>
-        <translation type="unfinished"></translation>
+        <translation>Wifi</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="63"/>
         <location filename="../qml/pages/MainPage.qml" line="75"/>
         <location filename="../qml/pages/MainPage.qml" line="87"/>
         <source>enabled</source>
-        <translation type="unfinished"></translation>
+        <translation>geactiveerd</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="63"/>
         <location filename="../qml/pages/MainPage.qml" line="75"/>
         <location filename="../qml/pages/MainPage.qml" line="87"/>
         <source>disabled</source>
-        <translation type="unfinished"></translation>
+        <translation>gedeactiveerd</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="64"/>
         <source>Current WiFi status</source>
-        <translation type="unfinished"></translation>
+        <translation>Huidige wifi status</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="75"/>
         <source>Hotspot</source>
-        <translation type="unfinished"></translation>
+        <translation>Hotspot</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="76"/>
         <source>Current Hotspot status</source>
-        <translation type="unfinished"></translation>
+        <translation>Huidige hotspot status</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="87"/>
         <source>KilL3r</source>
-        <translation type="unfinished"></translation>
+        <translation>KilL3r</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="88"/>
         <source>Current KilL3r status</source>
-        <translation type="unfinished"></translation>
+        <translation>Huidige KilL3r status</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="97"/>
         <location filename="../qml/pages/MainPage.qml" line="98"/>
         <source>Loading...</source>
-        <translation type="unfinished"></translation>
+        <translation>Laden...</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="99"/>
         <source>Last check</source>
-        <translation type="unfinished"></translation>
+        <translation>Laatste controle</translation>
     </message>
 </context>
 <context>
@@ -138,82 +151,82 @@ WiFiKilL3r app is created by TheYOSH https://theyosh.nl</source>
     <message>
         <location filename="../qml/pages/NetworksPage.qml" line="13"/>
         <source>About WiFiKilL3r</source>
-        <translation type="unfinished"></translation>
+        <translation>Over WiFiKilL3r</translation>
     </message>
     <message>
         <location filename="../qml/pages/NetworksPage.qml" line="17"/>
         <source>Manage trusted networks</source>
-        <translation type="unfinished"></translation>
+        <translation>Beheer vertrouwde netwerken</translation>
     </message>
     <message>
         <location filename="../qml/pages/NetworksPage.qml" line="21"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Instellingen</translation>
     </message>
     <message>
         <location filename="../qml/pages/NetworksPage.qml" line="37"/>
         <source>WiFiKilL3r</source>
-        <translation type="unfinished"></translation>
+        <translation>WiFiKilL3r</translation>
     </message>
     <message>
         <location filename="../qml/pages/NetworksPage.qml" line="54"/>
         <source>Current status</source>
-        <translation type="unfinished"></translation>
+        <translation>Huidige status</translation>
     </message>
     <message>
         <location filename="../qml/pages/NetworksPage.qml" line="63"/>
         <source>WiFi</source>
-        <translation type="unfinished"></translation>
+        <translation>Wifi</translation>
     </message>
     <message>
         <location filename="../qml/pages/NetworksPage.qml" line="63"/>
         <location filename="../qml/pages/NetworksPage.qml" line="75"/>
         <location filename="../qml/pages/NetworksPage.qml" line="87"/>
         <source>enabled</source>
-        <translation type="unfinished"></translation>
+        <translation>geactiveerd</translation>
     </message>
     <message>
         <location filename="../qml/pages/NetworksPage.qml" line="63"/>
         <location filename="../qml/pages/NetworksPage.qml" line="75"/>
         <location filename="../qml/pages/NetworksPage.qml" line="87"/>
         <source>disabled</source>
-        <translation type="unfinished"></translation>
+        <translation>gedeactiveerd</translation>
     </message>
     <message>
         <location filename="../qml/pages/NetworksPage.qml" line="64"/>
         <source>Current WiFi status</source>
-        <translation type="unfinished"></translation>
+        <translation>Huidige wifi status</translation>
     </message>
     <message>
         <location filename="../qml/pages/NetworksPage.qml" line="75"/>
         <source>Hotspot</source>
-        <translation type="unfinished"></translation>
+        <translation>Hotspot</translation>
     </message>
     <message>
         <location filename="../qml/pages/NetworksPage.qml" line="76"/>
         <source>Current Hotspot status</source>
-        <translation type="unfinished"></translation>
+        <translation>Huidige hotspot status</translation>
     </message>
     <message>
         <location filename="../qml/pages/NetworksPage.qml" line="87"/>
         <source>KilL3r</source>
-        <translation type="unfinished"></translation>
+        <translation>KilL3r</translation>
     </message>
     <message>
         <location filename="../qml/pages/NetworksPage.qml" line="88"/>
         <source>Current KilL3r status</source>
-        <translation type="unfinished"></translation>
+        <translation>Huidige KilL3r status</translation>
     </message>
     <message>
         <location filename="../qml/pages/NetworksPage.qml" line="97"/>
         <location filename="../qml/pages/NetworksPage.qml" line="98"/>
         <source>Loading...</source>
-        <translation type="unfinished"></translation>
+        <translation>Laden...</translation>
     </message>
     <message>
         <location filename="../qml/pages/NetworksPage.qml" line="99"/>
         <source>Last check</source>
-        <translation type="unfinished"></translation>
+        <translation>Laatste controle</translation>
     </message>
 </context>
 <context>
@@ -221,44 +234,44 @@ WiFiKilL3r app is created by TheYOSH https://theyosh.nl</source>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="20"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Instellingen</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="25"/>
         <source>Here you can enable or disable settings.</source>
-        <translation type="unfinished"></translation>
+        <translation>Hier kunt u instellingen activeren of deactiveren.</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="40"/>
         <source>Accesspoint MAC verification</source>
-        <translation type="unfinished"></translation>
+        <translation>Accesspoint MAC controle</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="40"/>
         <location filename="../qml/pages/SettingsPage.qml" line="51"/>
         <source>enabled</source>
-        <translation type="unfinished"></translation>
+        <translation>geactiveerd</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="40"/>
         <location filename="../qml/pages/SettingsPage.qml" line="51"/>
         <source>disabled</source>
-        <translation type="unfinished"></translation>
+        <translation>gedeactiveerd</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="41"/>
         <source>Current Accesspoint MAC verification status</source>
-        <translation type="unfinished"></translation>
+        <translation>Huidige Accesspoint MAC controle status</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="51"/>
         <source>Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Notificaties</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="52"/>
         <source>Current notification status</source>
-        <translation type="unfinished"></translation>
+        <translation>Huidige notificatie status</translation>
     </message>
 </context>
 </TS>
